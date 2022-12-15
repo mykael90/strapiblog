7848b3811b8b299088a4e1ec113442f27c8a1be3
Mykael Mello <eng.mykael@gmail.com>
2022-12-15 18:56:25 -0300

    SSL env

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME', 'blog-strapi'),
       user: env('DATABASE_USERNAME', 'postgres'),
       password: env('DATABASE_PASSWORD', 'senha'),
-      ssl: env.bool('DATABASE_SSL', true),
+      ssl: env.bool('DATABASE_SSL', false),
     },
   },
 });
